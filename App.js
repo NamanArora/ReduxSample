4dffc3ce49cdcd6033c1db13c28e4fb9bb7956f7
NamanArora <naman.arora11@gmail.com>
2017-11-19 17:32:35 +0530

    UI

--- App.js
+++ App.js
@@ -12,18 +12,64 @@ import {
   View
 } from 'react-native';
 
+import {connect} from 'react-redux'
+import { fetchPeopleFromAPI } from './actions'
+
+const {isFetching, people} = props.people
+
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        
-      </View>
+      <View style={container}>
+      <Text style={text}>Redux Example</Text>
+      <TouchableHighlight style={button} onPress={() => props.getPeople()}>
+        <Text style={buttonText}>Load People</Text>
+      </TouchableHighlight>
+      {
+        isFetching && <Text>Loading</Text>
+      }
+      {
+        people.length ? (
+          people.map((person, i) => {
+            return <View key={i} >
+              <Text>Name: {person.name}</Text>
+              <Text>Birth Year: {person.birth_year}</Text>
+            </View>
+          })
+        ) : null
+      }
+</View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: 100,
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  text: {
+    textAlign: 'center'
+  },
+  button: {
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0b7eff'
+  },
+  buttonText: {
+    color: 'white'
+}
 });
+
+mapStateToProps = (state) => 
+{
+  return {people: state.people}
+}
+mapDispatchToProps = (dispatch) => {
+  return { getPeople : () => dispatch(fetchPeopleFromAPI())}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
