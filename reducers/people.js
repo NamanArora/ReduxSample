da49bd45e594ecca3330a2c5c6cf2de7c1ccf40f
NamanArora <naman.arora11@gmail.com>
2017-11-19 16:57:01 +0530

    Add people reducer

--- reducers/people.js
+++ reducers/people.js
@@ -1,1 +1,30 @@
 import {FETCHING_PEOPLE, FETCHING_PEOPLE_SUCCESS, FETCHING_PEOPLE_FAILURE} from '../constants'
+
+
+const initialState = {
+    people: [],
+    isFetching: false,
+    error: false
+}
+
+export default function peopleReducer(state= initialState, action){
+    switch(action.type){
+        case FETCHING_PEOPLE:
+            return {
+                ...state,
+                isFetching: true
+            }
+        case FETCHING_PEOPLE_SUCCESS:
+        return {
+            ...state,
+            people: action.data,
+            isFetching: false
+        }
+        case FETCHING_PEOPLE_FAILURE:
+        return {
+            ...state,
+            isFetching: false,
+            error: true
+        }
+    }
+}
