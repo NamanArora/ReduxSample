2a9d83944d041b8b27662ac593eb2e0f61ca30e7
NamanArora <naman.arora11@gmail.com>
2017-11-19 15:38:57 +0530

    Hook up the provider

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './configureStore';
 
-AppRegistry.registerComponent('ReduxSample', () => App);
+const store = configureStore()
+
+const ReduxUI = () => {
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+}
+
+AppRegistry.registerComponent('ReduxSample', () =>ReduxUI);
